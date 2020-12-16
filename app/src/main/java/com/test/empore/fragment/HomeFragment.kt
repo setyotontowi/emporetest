dd87625d7fb6cfdfcf805e60ac31fa111d32c247
setyotontowi <setyotontowi@gmail.com>
2020-12-17 01:35:57 +0700

    Shimmer implemented

--- app/src/main/java/com/test/empore/fragment/HomeFragment.kt
+++ app/src/main/java/com/test/empore/fragment/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.test.empore.App
 import com.test.empore.R
+import com.test.empore.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -15,15 +16,30 @@ import com.test.empore.R
  */
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity?.application as App).appComponent.inject(this)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.placeholder.startShimmerAnimation()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.placeholder.stopShimmerAnimation()
     }
 
     companion object {
